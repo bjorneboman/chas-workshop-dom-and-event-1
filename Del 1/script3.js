a1f7e9c789c9676509daacdc793f3365518b13d0
bjorneboman <gostabohman@gmail.com>
2025-10-16 10:41:18 +0200

    Övning 3 DONE!

--- Del 1/script3.js
+++ Del 1/script3.js
@@ -2,3 +2,15 @@ const button1 = document.querySelector("#btn-1");
 const button2 = document.querySelector("#btn-2");
 const button3 = document.querySelector("#btn-3");
 
+button1.addEventListener("click", () => {
+    document.querySelector("h1").textContent = "Ny rubrik - nu händer det saker";
+})
+
+button2.addEventListener("click", () => {
+    document.querySelector("p").textContent = "Koda är som glass en varm sommardag!";
+})
+
+button3.addEventListener("click", () => {
+    document.querySelector("h1").style.color = "#dc2626";
+})
+
